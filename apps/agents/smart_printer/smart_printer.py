7e2f4bee743819cc430c609ab1d23bcad52ac476
Chongyang Xu <cxu@mpi-sws.org>
2024-12-20 12:03:19 +0100

    apps: agents: update smart_printer

--- apps/agents/smart_printer/smart_printer.py
+++ apps/agents/smart_printer/smart_printer.py
@@ -10,6 +10,8 @@ from llm_planner.agents.Llama3_8B import Llama3_8B
 
 from llm_planner.agents.Python import Python
 
+import time
+
 CODE_PROMPT = """Please help me summarize the function of following Agent Class from python package `llm_planner`
 
 {FILE_CODES}
@@ -40,6 +42,7 @@ You can use `llm_planner`, add new code, use other packages to finish the task
 You can generate new code to abtain resouces to full fill the request
 Make sure no format error for python code, no missing initilization, no missing import
 Make sure the python code can run and finish print for the reuqest.
+If the code finishes task sucessfuly, print word Done.
 """
 
 import os
@@ -99,11 +102,34 @@ class SmartPrinter(Agent):
         self.file_codes = read_python_files(directory_path)
         self.app_examples = read_single_file("..", "sot.py")
         self.app_examples += read_single_file("..", "mmlu.py")
-        self.print = Print(oai)
+        self.print = Print(oai, self)
+        self.max_retry = 3
+        self.num_retry = 0
 
     async def process(self, sender_id, message: Message):
 
-        if message["response"] is not None:
+        if message["response"] is None:
+            if message["retry"] is None:
+                code_prompt = CODE_PROMPT.format(FILE_CODES=self.file_codes)
+                msg = message.spawn()
+                msg["content"] = [{"role": "user", "content": code_prompt}]
+                msg["request"] = message["content"]
+                self.send(self.oai.id, msg)
+                # print(f"code_prompt:{code_prompt}")
+            else:
+                assert message["retry"] == True
+                self.num_retry += 1
+                if self.num_retry > self.max_retry:
+                    print("Exit at maximum retrying times")
+                else:
+                    code = message["code"]
+                    err = message["error"]
+                    retry_prompt = self.code_prompt_2 + "\n You have tried this code:\n```python\n{code}\n```\n\n This try failed with error: {err}\n"
+                    # self.code_prompt_2 = retry_prompt
+                    msg = message.spawn()
+                    msg["content"] = retry_prompt + "\nFix error and try again:\n"
+                    self.send(self.print.id, msg)
+        else:
             resp = message["response"]
             request = message["request_message"]["request"]
             code_prompt_2 = CODE_PROMPT2.format(
@@ -114,41 +140,50 @@ class SmartPrinter(Agent):
             )
             msg = message.spawn()
             msg['content'] = code_prompt_2
+            self.code_prompt_2 = code_prompt_2
             self.send(self.print.id, msg)
             # print(f"code_prompt2:{code_prompt_2}")
-        else:
-            code_prompt = CODE_PROMPT.format(FILE_CODES=self.file_codes)
-            msg = message.spawn()
-            msg["content"] = [{"role": "user", "content": code_prompt}]
-            msg["request"] = message["content"]
-            self.send(self.oai.id, msg)
-            # print(f"code_prompt:{code_prompt}")
 
 
 class Executer(Agent):
 
-    def __init__(self):
+    def __init__(self, sp):
         super().__init__()
         self.pyinterator = Python()
+        self.smart_printer = sp
 
     async def process(self, sender_id, message: Message):
         if message["response"] is None:
             code = extract_first_python_code(message["content"])
             message["content"] = code
+            with open("./temp/gen.py", 'w') as pycode_f:
+                    pycode_f.write(code)
             self.send(self.pyinterator.id, message)
         elif message["response"] == "ok":
             print("Done!")
         else:
-            err = message["response"]
-            print(f"Something wrong : {err}")
+            req_msg = message["request_message"]
+            assert req_msg is not None
+            msg = message.spawn()
+            msg["retry"] = True
+            msg["code"] = req_msg["content"]
+            msg["err"] = message["response"]
 
+            ts = int(time.time())
+            with open(f"./temp/gen_{ts}.py", 'w') as pycode_f:
+                    pycode_f.write(msg["code"])
+            with open(f"./temp/err_{ts}.txt", 'w') as pycode_f:
+                    pycode_f.write(msg["err"])
+
+            print(f"Something wrong, retrying...")
+            self.send(self.smart_printer.id, msg)
 
 class Print(Agent):
 
-    def __init__(self, oai):
+    def __init__(self, oai, sp):
         super().__init__()
         self.oai = oai
-        self.exe = Executer()
+        self.exe = Executer(sp)
 
     async def process(self, sender_id, message: Message):
         if message["response"] is not None:
@@ -158,13 +193,14 @@ class Print(Agent):
             self.send(self.exe.id, msg)
         else:
             code_prompt_2 = message["content"]
+            # print(code_prompt_2)
             msg = message.spawn()
             msg["content"] = [{"role": "user", "content": code_prompt_2}]
             self.send(self.oai.id, msg)
 
 
 # oai = OpenAI(max_token=512)
-oai = Anthropic(max_token=512)
+oai = Anthropic(max_token=1024)
 # oai = MiniLLM(max_token=1024)
 #oai = Llama3_8B(max_token=1024)
 sp = SmartPrinter(oai)
